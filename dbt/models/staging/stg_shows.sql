7c479d0151ab2323362b2a7b36118549f3d88c3b
Ayodele Akosile <a.akosile@gmail.com>
2026-03-29 15:54:34 +0000

    no longer need to rename columns for stg_shows with the addition of the itunes_found column. stg_shows now only pulls shows that have info from itunes

--- dbt/models/staging/stg_shows.sql
+++ dbt/models/staging/stg_shows.sql
@@ -1,8 +1,3 @@
-SELECT
-string_field_0 AS show_name,
-string_field_1 AS primary_genre,
-string_field_2 AS all_genres,
-string_field_3 AS genre_ids,
-string_field_4 AS publisher,
-
+SELECT *
 FROM {{ source('podcast_shows', 'raw_shows') }} s
+WHERE itunes_found = TRUE
